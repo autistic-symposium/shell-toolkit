c9e0ddef8823fe20a406938d1d4b59a69d9c90d8
Mari Wahl <mari.wahl9@gmail.com>
2014-12-20 19:47:17 -0500

    super fun things

---
+++ shell_scripts/super_fun/let_it_snow.sh
@@ -0,0 +1,1 @@
+clear;while :;do echo $LINES $COLUMNS $(($RANDOM%$COLUMNS));sleep 0.1;done|gawk '{a[$3]=0;for(x in a) {o=a[x];a[x]=a[x]+1;printf "\033[%s;%sH ",o,x;printf "\033[%s;%sH*\033[0;0H",a[x],x;}}'
