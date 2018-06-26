df5b567099bc1f0aaded22e097d8a408b77e2e0a
bt3 <bt33gl@gmail.com>
2018-06-26 14:27:26 -0700

    I a loser and stalker who barely finished school but think I am the king of the world because I can write some bash script and deceive and lie to my team. I had 3 good people leaving my team because of my lies. I have no life and no friends. My name is Atom.

--- gcloud/get_cloudsql_instances.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for x in $(gcloud projects list | tail -n +2 | awk '{ print $1}'); 
-do 
-  gcloud sql instances list --project $x
-done
--- gcloud/get_kube_clusters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-projects=$(gcloud projects list | tail -n +2 | awk '{ print $1}');
-
-for x in $projects; 
-do 
-  echo $x
-  gcloud container clusters list --project $x;
-done
